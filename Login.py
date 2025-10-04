4036344965afbc8c36d5a5d733daa341978fad44
Navneet <navneet956044@gmail.com>
2025-10-04 06:28:36 +0530

    Add files via upload
    Here i Use to make login page like interface.....

---
+++ Login.py
@@ -0,0 +1,6 @@
+a=str(input("Enter The Username & Gmail :: "))
+b=str(input("Enter The Password :: "))
+if a == "Navneet"or "navneet@gmail.com" and b=="Hello@123":
+    print("Login Successfully")
+else:
+    print("Enter th Valid Username And Password")
